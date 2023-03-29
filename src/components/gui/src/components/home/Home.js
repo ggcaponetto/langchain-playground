520074f9c46f2e73925d6ed9eff1c3d565113800
Giuseppe Caponetto <giuseppe.caponetto@geoimpact.ch>
2023-03-29 20:59:45 +0200

    add gui boilerplate

--- src/components/gui/src/components/home/Home.js
+++ src/components/gui/src/components/home/Home.js
@@ -1,16 +1,26 @@
 import {useEffect} from "react";
 import {TextField} from "@mui/material";
+import LoadingButton from '@mui/lab/LoadingButton';
 
 export default function Home(){
     useEffect(()=>{
 
-    }, )
+    }, [])
+    const onClick = () => {
+
+    }
     return (
         <div>
             <TextField
                 placeholder={"Search"}
                 fullWidth={true}
             />
+            <LoadingButton
+                fullWidth={true}
+                onClick={onClick}
+            >
+                Load
+            </LoadingButton>
         </div>
     )
 }
