5bbc98932c081bfcd391dc7b3b10088b6ffbe107
Giuseppe Capnetto <ggcaponetto@gmail.com>
2023-03-31 20:12:51 +0200

    wip

--- src/components/web-scraper/src/scraper.js
+++ src/components/web-scraper/src/scraper.js
@@ -4,53 +4,64 @@ const puppeteer = require('puppeteer');
 async function extractLinks(url) {
     const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
-    await page.goto(url);
-    const links = (await page.evaluate(() => {
+    await page.goto(url, {
+        waitUntil: "domcontentloaded"
+    });
+    const links = await page.evaluate(() => {
         const linkElements = Array.from(document.querySelectorAll('a'));
         return linkElements.map(link => link.href);
-    })).filter(link => link.startsWith("http"));
+    });
     await browser.close();
-    console.log(`extracted ${links.length} links`)
+    // console.log(`extracted ${links.length} links`)
     return links;
 }
 
-// Step 3
-async function crawl(url, visitedUrls = new Set(), options) {
-    let allLinks = await extractLinks(url);
-    const links = allLinks.splice(options.maxLinksPerSite);
-    console.log(`crawling the first ${options.maxLinksPerSite} of the ${allLinks.length} links`)
-    for (const link of links) {
-        await crawl(link, visitedUrls, options);
-        console.log("crawled", link)
-        visitedUrls.add(url);
-    }
-}
-
 // Step 4
-async function crawlSite(url) {
-    const masterList = [url];
-    const maxLinksPerSite = 2;
-    const maxMasterListSize = 10;
-    let currentIndex = 0;
-    while ((currentIndex < masterList.length) && masterList.length <= maxMasterListSize) {
-        const currentUrl = masterList[currentIndex];
-        await crawl(
-            currentUrl,
-            masterList.length > 0 ? new Set(masterList) : new Set(),
-            {
-                maxLinksPerSite,
-                maxMasterListSize
+async function crawlSite(url, options = {
+    maxLinksOnUrl: 5,
+    maxDepth: 0,
+    currentDepth: 0,
+    urlAccumulator: []
+}) {
+    let depth = options.currentDepth;
+    let currentUrl = url;
+    let urls = await extractLinks(currentUrl, options);
+    const isAtMaxDepth = depth === options.maxDepth;
+    // console.log(`Got ${urls.length} urls on depth ${depth}.`, urls);
+    let filteredUrls = [...new Set(urls.filter(
+        url => url.includes(currentUrl) && url.includes("#") === false && currentUrl !== url
+    ))].splice(0, options.maxLinksOnUrl)
+    console.log(`Got ${filteredUrls.length} filtered urls from ${url} on depth ${depth}`, filteredUrls);
+
+    if(isAtMaxDepth){
+        console.log(`Finished crawling on depth ${depth}/${options.maxDepth}.`, options);
+        return options;
+    } else {
+        let promises = []
+        for(const newUrl of filteredUrls){
+            if(
+                options.urlAccumulator.includes(newUrl) === false
+            ){
+                let tempOption = {
+                    ...options,
+                    currentDepth: depth + 1,
+                    urlAccumulator: [...new Set([
+                        ...options.urlAccumulator,
+                        newUrl
+                    ])]
+                }
+                promises.push(await crawlSite(newUrl, tempOption))
             }
-        );
-        const newLinks = (await extractLinks(currentUrl)).splice(maxLinksPerSite);
-        newLinks.forEach(link => {
-            if (!masterList.includes(link)) {
-                masterList.push(link);
-            }
-        });
-        currentIndex++;
+        }
+        let depthResults = await Promise.all(promises)
+        return depthResults
     }
 }
 
 // Step 5
-crawlSite('https://www.swissenergyplanning.ch');
+crawlSite('https://www.squarespace.com/', {
+    maxLinksOnUrl: 5,
+    maxDepth: 1,
+    currentDepth: 0,
+    urlAccumulator: []
+});
