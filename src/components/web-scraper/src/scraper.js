9c07c683aea1f7bb2f5fc818b7d9cb87c0bb104f
Giuseppe Caponetto <giuseppe.caponetto@geoimpact.ch>
2023-04-01 11:24:44 +0200

    wip

--- src/components/web-scraper/src/scraper.js
+++ src/components/web-scraper/src/scraper.js
@@ -2,7 +2,7 @@ const puppeteer = require('puppeteer');
 
 // Step 2
 async function extractLinks(url) {
-    const browser = await puppeteer.launch({ headless: false });
+    const browser = await puppeteer.launch({ headless: true });
     const page = await browser.newPage();
     await page.goto(url, {
         waitUntil: "domcontentloaded"
@@ -17,51 +17,63 @@ async function extractLinks(url) {
 }
 
 // Step 4
-async function crawlSite(url, options = {
-    maxLinksOnUrl: 5,
-    maxDepth: 0,
-    currentDepth: 0,
-    urlAccumulator: []
-}) {
-    let depth = options.currentDepth;
-    let currentUrl = url;
-    let urls = await extractLinks(currentUrl, options);
-    const isAtMaxDepth = depth === options.maxDepth;
-    // console.log(`Got ${urls.length} urls on depth ${depth}.`, urls);
-    let filteredUrls = [...new Set(urls.filter(
-        url => url.includes(currentUrl) && url.includes("#") === false && currentUrl !== url
-    ))].splice(0, options.maxLinksOnUrl)
-    console.log(`Got ${filteredUrls.length} filtered urls from ${url} on depth ${depth}`, filteredUrls);
-
+async function crawlSite(url, options) {
+    const isAtMaxDepth = options.currentDepth === options.maxDepth;
     if(isAtMaxDepth){
-        console.log(`Finished crawling on depth ${depth}/${options.maxDepth}.`, options);
-        return options;
+        options.results = [
+            ...new Set([
+                ...options.results,
+                url
+            ])
+        ]
+        console.log(`Finished crawling on depth ${options.currentDepth}/${options.maxDepth}.`, options);
+        return new Promise((res) => {res(options)})
     } else {
-        let promises = []
+        let urls = await extractLinks(url, options);
+        // console.log(`Got ${urls.length} urls on depth ${depth}.`, urls);
+        let filteredUrls = [...new Set(urls.filter(
+            myUrl => myUrl.includes(url) && myUrl.includes("#") === false && url !== myUrl
+        ))].splice(0, options.maxLinksOnUrl)
+        console.log(`Got ${filteredUrls.length} filtered urls from ${url} on depth ${options.currentDepth}`, filteredUrls);
+        const promises = [];
         for(const newUrl of filteredUrls){
             if(
                 options.urlAccumulator.includes(newUrl) === false
             ){
                 let tempOption = {
                     ...options,
-                    currentDepth: depth + 1,
+                    currentDepth: options.currentDepth + 1,
                     urlAccumulator: [...new Set([
                         ...options.urlAccumulator,
                         newUrl
-                    ])]
+                    ])],
+                    results: [
+                        ...new Set([
+                            ...options.results,
+                            newUrl
+                        ])
+                    ]
                 }
-                promises.push(await crawlSite(newUrl, tempOption))
+                const tempPromise = crawlSite(newUrl, tempOption).then(r => r);
+                promises.push(tempPromise);
             }
         }
-        let depthResults = await Promise.all(promises)
-        return depthResults
+        let results = []
+        for (const promise of promises) {
+            results.push(await Promise.resolve(promise))
+        }
+        return results;
     }
 }
 
 // Step 5
-crawlSite('https://www.squarespace.com/', {
-    maxLinksOnUrl: 5,
-    maxDepth: 1,
-    currentDepth: 0,
-    urlAccumulator: []
-});
+(async ()=>{
+    let crawlingResult = await crawlSite('https://www.squarespace.com/', {
+        maxLinksOnUrl: 5,
+        maxDepth: 3,
+        currentDepth: 0,
+        urlAccumulator: [],
+        results: []
+    });
+    console.log(`Crawling result`, crawlingResult)
+})()
